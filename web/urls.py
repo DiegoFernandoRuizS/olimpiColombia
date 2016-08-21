eec19fbb5df646c90217fd3192e09876560db622
Diego F. Ruiz S <yego23@gmail.com>
2016-08-21 12:28:17 -0500

    Template athlete
    -- First version athlete template with url and add field video on model
    athlete

--- web/urls.py
+++ web/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
-from .views import IndexView
+from .views import IndexView,AthleteView
+from web import views
 
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name='index'),
+    url(r'^athlete/$', AthleteView.as_view(), name='athlete'),
 
 ]
