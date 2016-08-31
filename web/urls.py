55258db50177b86128cf36f02c50b7bac9822967
ivantn <Bizagi07>
2016-08-30 22:34:56 -0500

    test social oauth, fix requirements

--- web/urls.py
+++ web/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     url(r'^athletes_by_sport/(?P<sport_name>[^/]+)/$', AthletesBySportList.as_view(), name='athletes_by_sport'),
     url(r'^schedules/(?P<athlete_id>\w+)', ScheduleList.as_view(), name='schedule_by_athlete'),
     url(r'^login/', LoginView.as_view(), name='login'),
-    url(r'^complete/google-oauth2', IndexView.as_view(), name='index'),
+
 ]
