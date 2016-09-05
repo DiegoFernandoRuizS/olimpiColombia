ff0be3bf0937ea79fff9f53efb9db28df97d7443
soybackend <josealvarez712@gmail.com>
2016-09-05 14:03:56 -0500

    newrelic added

--- olimpiColombia/wsgi.py
+++ olimpiColombia/wsgi.py
@@ -1,7 +1,13 @@
 import os
 
+import newrelic.agent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+newrelic.agent.initialize('newrelic.ini')
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "olimpiColombia.settings")
 
 application = get_wsgi_application()
+application = newrelic.agent.wsgi_application()(application)
