ecf75fa49dbe750b5b48f0afca218059ea06806d
ivantn <Bizagi07>
2016-09-06 23:56:12 -0500

    angular webpack

--- WebContent/app/restApi/sportsApiService.js
+++ WebContent/app/restApi/sportsApiService.js
@@ -2,7 +2,7 @@ var restApiModule = angular.module('restApiModule');
 restApiModule.factory('SportsApiService', ['$resource', function ($resource) {
     return $resource('api/sports', {}, {
         loadSports: {
-            url: 'api/sports/?format=json',
+            url: 'https://olimpi-colombia.herokuapp.com/api/sports/?format=json',
             method: 'GET',
             isArray: true,
             params: {}
