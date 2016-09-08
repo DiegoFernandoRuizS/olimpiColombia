daa951a7baa9ca2a56706dfb3b4baa841bd6e0bf
ivantn <Bizagi07>
2016-09-08 10:56:54 -0500

    fix images an video

--- WebContent/app/sportList/sportListService.js
+++ WebContent/app/sportList/sportListService.js
@@ -3,6 +3,7 @@ sportListModule.factory('SportListService', ['SportsApiService', function (Sport
     var Sports = function () {
         this.sports = [];
         this.athletesBySport = [];
+        this.athletesBySportEmpty = false;
         this.error = null;
         this.getSports = function () {
             var self = this;
@@ -15,9 +16,13 @@ sportListModule.factory('SportListService', ['SportsApiService', function (Sport
             var self = this;
             self.athletesBySport = [];
             self.error = null;
+            self.athletesBySportEmpty = false;
             SportsApiService.loadAthletesBySport({sportName: sport.name},
                 function (response) {
                     self.athletesBySport = response;
+                    if (self.athletesBySport.length === 0) {
+                        self.athletesBySportEmpty = true;
+                    }
                 },
                 function (error) {
                     if (error.status = 403) {
